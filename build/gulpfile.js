994b3aef4ea2322f4fecbd4d79e6a010c640684a
sutter-dave <sutter@intransix.com>
2021-02-10 18:26:38 -0800

    work in progress on build system

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,60 +1,26 @@
-const { src, dest, series, parallel, task } = require('gulp');
-const releasePackageJson = require("../package.json");
+const {series, parallel, src, dest} = require('gulp');
 const rollup = require('rollup');
-//const createResolveIdPlugin = require("../../apogeejs-admin/build-src/absoluteRefPluginNEW.js");
-//const createResolveIdPlugin = require("./absoluteRefPluginNEW.js");
-const fs = require("fs");
+const {getJsFileHeader, getReleaseFolder, makeSureReleaseNotPresent, createResolveId} = require("../../apogeejs-admin/build-src/build-utils.js");
+
+const releasePackageJson = require("../package.json");
 
 //read data from the release package.json
 let isDevRelease = releasePackageJson.DEV_RELEASE;
 let version = releasePackageJson.version;
+let repoName = releasePackageJson.name;
 let esModuleFileName = releasePackageJson.module;
 let npmModuleFileName = releasePackageJson.main;
-let repoName = npmModuleFileName;
-let dependencies = releasePackageJson.dependencies;
 
 //for absolute references
 const PATH_TO_ABSOLUTE_ROOT = "../..";
-//let resolveId = createResolveIdPlugin(__dirname,PATH_TO_ABSOLUTE_ROOT,dependencies);
+let resolveId = createResolveId(__dirname,PATH_TO_ABSOLUTE_ROOT);
 
-//IMPORT THIS!!!
-
-//version header for js files
-function getJsFileHeader(fileName) {
-    return "// File: " + fileName + "\n" +
-        "// Version: " + version + "\n" +
-        "// Copyright (c) 2016-2021 Dave Sutter\n" + 
-        "// License: MIT\n";
-}
-
-//IMPORT THIS!!!
-
-//==============================
-// Top Level Values
-//==============================
-const RELEASE_PARENT_FOLDER = "../../apogeejs-admin";
-const RELEASE_FOLDER = isDevRelease ?  "releases-dev" : "releases";
-const RELEASE_NAME = "v" + version;
-const OUTPUT_FOLDER = RELEASE_PARENT_FOLDER + "/" + RELEASE_FOLDER + "/" + repoName + "/" + RELEASE_NAME;
-
-
-//IMPORT THIS!!!
-//const RELEASE_FOLDER = getReleaseFolder(versionConfig);
-
+const outputFolder = getReleaseFolder(repoName,version,isDevRelease);
 
 //======================================
 // Release Info
 //======================================
 
-//IMPORT THIS!!!
-function makeSureReleaseNotPresent() {
-    return new Promise( (resolve,reject) => {
-        fs.stat(OUTPUT_FOLDER, (err, stats) => {
-            if (err) resolve("File is not present!");
-            else reject("Release is already present! If this should not be true, check the version numbers");
-        });
-    })
-}
 
 //base files - version info
 const BASE_FILES = [
@@ -62,50 +28,45 @@ const BASE_FILES = [
 ]
 
 let copyReleaseInfoTask = parallel(
-    () => copyFilesTask(BASE_FILES,OUTPUT_FOLDER)
+    () => copyFilesTask(BASE_FILES,outputFolder)
 )
 
+/** This function is a gulp task that copies files to a destination folder. */
+function copyFilesTask(fileList,destFolder) {
+    return src(fileList,{allowEmpty: true})
+        .pipe(dest(destFolder));
+}
+
+
 //==============================
-// Web App
+// Library Module
 //==============================
 
 function packageLibTask() {
     return rollup.rollup({
         input: '../src/' + esModuleFileName,
         plugins: [
-//            {resolveId}
+            {resolveId}
         ]
     }).then(bundle => {
         return Promise.all([
             bundle.write(
                 { 
-                    file: OUTPUT_FOLDER + "/" + esModuleFileName,
+                    file: outputFolder + "/" + esModuleFileName,
                     format: 'es',
-                    banner: getJsFileHeader(esModuleFileName)
+                    banner: getJsFileHeader(esModuleFileName,version)
                 }
             ),
             bundle.write(
                 { 
-                    file: OUTPUT_FOLDER + "/" + npmModuleFileName,
+                    file: outputFolder + "/" + npmModuleFileName,
                     format: 'cjs',
-                    banner: getJsFileHeader(npmModuleFileName)
+                    banner: getJsFileHeader(npmModuleFileName,version)
                 }
             )
         ]);
     });
 }
-
-//==============================
-// Utility Tasks
-//==============================
-
-//copy files utility function
-//IMPORT THIS!!!
-function copyFilesTask(fileList,destFolder) {
-    return src(fileList,{allowEmpty: true})
-        .pipe(dest(destFolder));
-}
-
 
 //============================
 // Exports
@@ -113,9 +74,11 @@ function copyFilesTask(fileList,destFolder) {
 
 //This task executes the complete release
 exports.release = series(
-    makeSureReleaseNotPresent,
+    () => makeSureReleaseNotPresent(outputFolder),
     parallel(
         copyReleaseInfoTask,
         packageLibTask
     )
 );
+
+
